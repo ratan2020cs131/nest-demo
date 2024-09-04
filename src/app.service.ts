2184b005eade7484f0c0b07c91ddbf0ad35191f9
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2024-09-04 11:00:18 +0530

    fix gql module (#9)

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+
+interface HelloResponse {
+  message: string;
+}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): HelloResponse {
+    return {
+      message:
+        "Hello! This project is build with Serverless, Nest.js, GraphQL, PostgreSQL",
+    };
   }
 }
--- src/graphql.module.ts
+++ src/graphql.module.ts
@@ -1,13 +1,16 @@
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
-import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { join } from "path";
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), "src/schema.graphql"),
+      autoSchemaFile:
+        process.env.NODE_ENV === "production"
+          ? true
+          : join(process.cwd(), "src/schema.gql"),
     }),
   ],
 })
